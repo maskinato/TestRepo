44783a4b6b08a9f6b3eac735f58d877e5bc8e1bc
Mas Kinato <mas.kinato+LTK@listrak.com>
2025-03-15 09:21:04 -0400

    adding another instance

--- Performanceinsight.tf
+++ Performanceinsight.tf
@@ -5,3 +5,10 @@ monitoring_interval             = "1"
  ## INF-2710 this retention is needed to activate Advanced performance insights, more info on ticket
  performance_insights_retention_period = 465\
 
+
+enabled_cloudwatch_logs_exports = ["postgresql"]
+monitoring_interval             = "1"
+ performance_insights_enabled    = true
+ monitoring_role_arn             = arn:aws:iam::044394465464:role/mas-contactEnhancedMonitoringRole
+ ## INF-2710 this retention is needed to activate Advanced performance insights, more info on ticket
+ performance_insights_retention_period = 465\
